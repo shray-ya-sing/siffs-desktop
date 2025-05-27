b9129fae6e4d8553b30ee72d5b5cb801b5f59e2a
cori-tan <ss6708@columbia.edu>
2025-05-27 17:33:44 -0400

    installed tailwindcss

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -3,9 +3,22 @@ import type { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
+// Remove the existing CSS rule if it exists
+const cssRuleIndex = rules.findIndex(rule => 
+  rule && typeof rule === 'object' && 'test' in rule && rule.test.toString().includes('.css')
+);
+if (cssRuleIndex !== -1) {
+  rules.splice(cssRuleIndex, 1);
+}
+
+// Add the new CSS rule with postcss-loader
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [
+    { loader: 'style-loader' },
+    { loader: 'css-loader' },
+    { loader: 'postcss-loader' },
+  ],
 });
 
 export const rendererConfig: Configuration = {
