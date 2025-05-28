893218ab6e8340216cb032170e575c99846606f0
cori-tan <ss6708@columbia.edu>
2025-05-28 05:58:33 -0400

    fix: Hash Router for routing instead of browser  - -fixes packaged app routing issue

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -29,7 +29,7 @@
 
 import React from 'react';
 import { createRoot, Root } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { App } from './renderer/App';
 import './index.css';
 import './renderer/styles/globals.css';
@@ -50,11 +50,11 @@ if (container) {
   
   root.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/*" element={<App />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   );
 }
